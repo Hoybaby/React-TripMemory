41404416bdb19c8e20f97800a9779bc407bb996a
Hoybaby <mbartek436@gmail.com>
2021-09-06 19:47:18 -0400

    made a simple format for the posts/forms so it can display properly onto my app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 
-import './App.css';
+// import './App.css';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import memories from './images/memories.png'
+
 
 function App() {
   return (
@@ -10,6 +15,19 @@ function App() {
           Memories
           <img src={memories} alt="memories" height="60"/>
         </Typography>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alighnItems="stretch" spacing={3}>
+              {/* the xs means for small devices it will take it all up */}
+              <Grid item xs={12} sm={7}>
+                <Posts/>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form/>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
       </AppBar>
 
     </Container>
