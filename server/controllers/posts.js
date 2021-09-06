e28d1d62a95b207c62be09dc3e9f6fe6d2829d9e
Hoybaby <mbartek436@gmail.com>
2021-09-06 12:49:05 -0400

    made the logic for the createPost in the controllers/post.js folder. this will input the data from the front into the schema i have created earlier

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,7 +6,7 @@ export const getPosts = async (req,res) => {
     try {
 
         const postMessages = await PostMessage.find();
-        console.log(postMessages)
+        // console.log(postMessages)
         res.status(200).json(postMessages)
 
     } catch(error) {
@@ -14,6 +14,20 @@ export const getPosts = async (req,res) => {
     }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
+export const createPost = async (req, res) => {
+
+    const post = req.body;
+
+    // this PostMessage is the schema that is in the models which will get formatted from a simple form down the road
+    const newPost = new PostMessage(post)
+
+    try {
+        // this is an asynchrnous action
+        await newPost.save()
+
+        res.status(201).json(newPost)
+    } catch(error) {
+        res.status(409).json({message: error.message});
+
+    }
 }
