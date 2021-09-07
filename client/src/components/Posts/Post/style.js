0e420f38d8624c44122794b86ca62408f89dadd7
Hoybaby <mbartek436@gmail.com>
2021-09-06 22:27:22 -0400

    adding some styling to the singular post so when a user inputs the information, it looks good

--- client/src/components/Posts/Post/style.js
+++ client/src/components/Posts/Post/style.js
@@ -1,1 +1,30 @@
 import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles({
+    media: {
+        height: 0,
+        paddingTop: '56.25%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        backgroundBlendMode: 'darken',
+    },
+    border: {
+        border: 'solid',
+    },
+    fullHeightCard: {
+        height: '100%',
+    },
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        borderRadius: '15px',
+        height: '100%',
+        position: 'relative',
+    },
+    overlay: {
+        position: 'absolute',
+        top: '20px',
+        left: '20px',
+        color: 'white',
+    },
+})
