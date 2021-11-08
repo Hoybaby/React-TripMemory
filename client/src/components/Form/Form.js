4b1989d9b71ac04a17364e02d24a652748c3685b
Hoybaby <mbartek436@gmail.com>
2021-11-07 22:47:14 -0500

    making the text fields for creator, title, tags and the message

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -24,13 +24,40 @@ const Form =() => {
                 <Typography variant="h6" >Creating a Memory</Typography> 
                 <TextField 
                     name="creator"
-                    variant="outlind" 
+                    variant="outlined" 
                     label="Creator" 
                     fullWidth
                     value={postData.creator}
                     // the whole data will be stored in the state and each object key will be a different text field
                     onChange={(e) => setPostData({...postData, creator: e.target.value})}
+                />
+                <TextField 
+                    name="title"
+                    variant="outlined" 
+                    label="Title" 
+                    fullWidth
+                    value={postData.title}
+                    // the whole data will be stored in the state and each object key will be a different text field
+                    onChange={(e) => setPostData({...postData, title: e.target.value})}
                 /> 
+                <TextField 
+                    name="message"
+                    variant="outlined" 
+                    label="Message" 
+                    fullWidth
+                    value={postData.message}
+                    // the whole data will be stored in the state and each object key will be a different text field
+                    onChange={(e) => setPostData({...postData, message: e.target.value})}
+                /> 
+                <TextField 
+                    name="tags"
+                    variant="outlined" 
+                    label="Tags" 
+                    fullWidth
+                    value={postData.tags}
+                    // the whole data will be stored in the state and each object key will be a different text field
+                    onChange={(e) => setPostData({...postData, tags: e.target.value})}
+                />  
             </form>
         </Paper>
     )
