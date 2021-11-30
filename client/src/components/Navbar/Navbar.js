2d95ce1abb08ee6572c808d0244cae52f01267fc
Hoybaby <mbartek436@gmail.com>
2021-11-30 13:02:31 -0500

    trying to make some changes to the appbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AppBar, Typography } from '@material-ui/core'
+
+const Navbar = () => {
+    return (
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <Typography className={classes.heading }variant="h2" align="center">
+                Memories
+                <img className={classes.image} src={memories} alt="memories" height="60"/>
+            </Typography>
+        </AppBar>
+    )
+}
+
+export default Navbar
