23107494d827f5a118ded209e0ed227c04ece896
Hoybaby <mbartek436@gmail.com>
2021-11-30 14:52:03 -0500

    made the browserRouter and the switch for the routing of the different components on click. I have not yet made the Auth part but will shortly

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 
 // import './App.css';
-import React, {useState, useEffect} from 'react';
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
-import {getPosts} from './actions/posts'
-import Posts from './components/Posts/Posts'
-import Form from './components/Form/Form'
+import React from 'react';
+import {Container} from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // import memories from './images/memories.png'
-import useStyles from './styles';
+
 import './index.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
@@ -19,24 +14,19 @@ import Home from './components/Home/Home';
 
 const App = () => {
 
-  const [currentId, setCurrentId] = useState(null)
-
-  const classes= useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-
-    // the reason i am adding currentId is because since now we are clearing the input, we are changing the currentId which then will change the backend post.
-  }, [currentId, dispatch])
-
   return (
-    <Container maxWidth="lg">
-      <Navbar />
-      <Home/>
-      
+    <BrowserRouter>
 
-    </Container>
+      <Container maxWidth="lg">
+        <Navbar />
+        {/* the switch contains the routes */}
+        <Switch> 
+          <Route exact path="/" component={Home} />
+          <Route path="/auth" component={Auth} />
+        </Switch>
+        <Home/>
+      </Container>
+    </BrowserRouter>
 
   );
 }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Container, Grow, Grid } from '@material-ui/core';
 
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
-
+import { useDispatch } from 'react-redux';
+import {getPosts} from '../../actions/posts'
 
 const Home = () => {
 
