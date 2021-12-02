b0f38b14804fd18e14b015513a92bd931c6f5d74
Hoybaby <mbartek436@gmail.com>
2021-12-01 23:29:24 -0500

    making different fields but wondering if i could make a custom component to handle this logic with states

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -12,13 +12,35 @@ const Auth = () => {
 
     const isSignup = false;
 
+    const handleSubmit = () => {
+        console.log('submit');
+    }
+
+    const handleChange = () => {
+        console.log('change');
+    }
+
     return (
         <Container compononent="main" maxWidth="sm">
             <Paper className={classes.paper} elevation={3}>
                 <Avatar className={classes.avatar}>
                     <LockOutlinedIcon />
                 </Avatar>
-                <Typography variant="h5"> Sign In </Typography>
+                {/* using logic and iterator to determine if user is not signed up it will show Sign up */}
+                <Typography variant="h5">{isSignup? 'Sign Up' : 'Sign in'} </Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}> 
+                        {
+                            // if is SIgnup is true then it will show the following, which be two different inputs
+                            isSignup && (
+                                <>
+                                    <TextField name="firstName" label="First Name" handleChange={handleChange} autoFocus xs={6}/>
+                                    <TextField name="lastName" label="Last Name" handleChange={handleChange} autoFocus xs={6}/>
+                                </>
+                            )
+                        }
+                    </Grid>
+                </form>
             </Paper>
         </Container>
     )
