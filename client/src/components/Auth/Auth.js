db0d007baaa76a4b18a51c8cd5258fbd71e6a28b
Hoybaby <mbartek436@gmail.com>
2021-12-02 14:34:36 -0500

    made the button functional for sign up and close

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Avatar, Button, Paper, Grid, Typography, Container, Typography } from '@material-ui/core';
+import { Avatar, Button, Paper, Grid, Typography, Container} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Input from './Input';
 
@@ -11,16 +11,28 @@ const Auth = () => {
     // this is going to handle if we are going to show our password later
     const [ showPassword, setShowPassword ] = useState(false);
 
+    const[isSignup, setIsSignup] = useState(false);
+
     const state = null;
 
-    const isSignup = false;
+    // const isSignup = true;
 
     const handleSubmit = () => {
         console.log('submit');
     }
 
+    // we need a prev state. when we are changing the state of the old state, we need to call the prev
+    // so this will be able to toggle the password
+    const handleShowPassword = () => setShowPassword(( prevShowPassword => !prevShowPassword));
+
     const handleChange = () => {
         console.log('change');
+    }
+
+    const switchMode = () => {
+        setIsSignup(prevIsSignup => !prevIsSignup);
+        handleShowPassword(false);
+
     }
 
     return (
@@ -46,6 +58,19 @@ const Auth = () => {
                         <Input name="email" label="Email address" type="email" handleChange={handleChange} autoFocus />
                         {/* so the input below will check the state before deciding what kind of type it should be */}
                         <Input name="password" label="Password" type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+                        { isSignup && <Input name="confirmPassword" label="Repeat Password" type="password" handleChange={handleChange} />}
+                    </Grid>
+                    {/* button will display two kind of texts */}
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        {isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Button onClick={switchMode}> 
+                                {isSignup? 'Already have an account? Sign in' : 'Don\'t have an account? Sign up'}
+                            </Button>
+                        </Grid>
+
                     </Grid>
                 </form>
             </Paper>
