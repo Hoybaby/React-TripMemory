02a9a389e5c10ff65d2738562e8d50d6b88075a7
Hoybaby <mbartek436@gmail.com>
2021-12-03 12:02:44 -0500

    implemented the logout feature but I see that it still retrieves from the localstorage so it is not dispatching properly

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import { AppBar, Avatar, Toolbar, Typography, Button } from '@material-ui/core';
+
+import { useDispatch } from 'react-redux';
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
@@ -12,6 +14,9 @@ const Navbar = () => {
     
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     console.log(user);
 
     useEffect(() => {
@@ -22,6 +27,17 @@ const Navbar = () => {
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, []);
 
+    const logout =() => {
+        dispatch({
+            type: 'LOGOUT'
+        });
+
+        navigate('/');
+
+        setUser(null);
+
+
+    }
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
@@ -38,7 +54,7 @@ const Navbar = () => {
                     <div className={classes.profile}>  
                         <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
                         <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                     </div>
                 ) : (
                     <Button component={Link} to='/auth' variant="contained" color="primary"> Sign In</Button>
